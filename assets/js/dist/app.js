71ddaa8052f880a9e85f21df992282f512a54cdd
Scott Rees <srees@worldvision.org>
2016-06-04 19:28:22 -0700

    feat: finish implementing responsive design and click events for eatery objects

--- assets/js/dist/app.js
+++ assets/js/dist/app.js
@@ -23,9 +23,22 @@ var Eateries = function () {
     key: 'getEateriesJSON',
     value: function getEateriesJSON() {
       var that = this;
-      jQuery.ajax('assets/js/src/data.json').done(function (data) {
-        that.rawData = JSON.parse(data);
-        that.buildEateriesArray();
+      var defaultMessage = 'Sorry! We could not locate the data. Please try again later.';
+
+      jQuery.ajax('assets/js/src/data.json').fail(function (response) {
+        errorMessageHandler.displayError(defaultMessage);
+      }).done(function (data) {
+        if (data) {
+          try {
+            that.rawData = JSON.parse(data);
+            that.buildEateriesArray();
+          } catch (e) {
+            errorMessageHandler.displayError('Sorry! Parsing Data failed. Please try again later.'); //error in the above string(in this case,yes)!
+            return false;
+          }
+        } else {
+          errorMessageHandler.displayError(defaultMessage);
+        }
       });
     }
   }, {
@@ -41,14 +54,16 @@ var Eateries = function () {
     value: function buildEatery(business, eateryObject) {
       var that = this;
       that.data.push({
+        'id': that.data.length,
         'name': ko.observable(eateryObject.name),
         'addressLine1': ko.observable(eateryObject.location.address[0]),
-        'addressLine2': ko.observable(eateryObject.location.city),
+        'addressLine2': ko.observable(eateryObject.location.city + ', ' + eateryObject.location.state_code + ' ' + eateryObject.location.postal_code),
         'website': ko.observable(business.website),
         'logoImage': ko.observable(business.logo),
         'rating': ko.observable(eateryObject.rating),
         'lat': ko.observable(eateryObject.location.coordinate.latitude),
-        'long': ko.observable(eateryObject.location.coordinate.longitude)
+        'long': ko.observable(eateryObject.location.coordinate.longitude),
+        'isSelected': ko.observable(false)
       });
 
       //All data loaded
@@ -61,10 +76,24 @@ var Eateries = function () {
     key: 'getYelpData',
     value: function getYelpData(business) {
       var that = this;
+      var defaultMessage = 'Sorry! Retrieving Yelp Data failed. Please try again later.';
       jQuery.ajax('api/getYelpBusiness.php', {
         data: { business: business.name }
-      }).done(function (eateryObject) {
-        that.buildEatery(business, JSON.parse(eateryObject));
+      }).fail(function (response) {
+        errorMessageHandler.displayError(defaultMessage);
+      }).done(function (response) {
+        if (response) {
+          var eateryJSON = {};
+          try {
+            eateryJSON = JSON.parse(response);
+          } catch (e) {
+            errorMessageHandler.displayError('Sorry! Parsing Yelp Data failed. Please try again later.'); //error in the above string(in this case,yes)!
+            return false;
+          }
+          that.buildEatery(business, eateryJSON);
+        } else {
+          errorMessageHandler.displayError(defaultMessage);
+        }
       });
     }
   }]);
@@ -81,8 +110,35 @@ var Eateries = function () {
 var eateriesViewModel = {
   allEateries: ko.observableArray(),
   init: function init() {
-    eateriesViewModel.allEateries = ko.observableArray(eateriesObject.data);
-    ko.applyBindings(eateriesViewModel, document.getElementById('Eateries'));
+    var that = this;
+    this.allEateries = ko.observableArray(eateriesObject.data);
+    this.currentNameSearch = ko.observable("");
+
+    this.filterEateries = ko.computed(function () {
+      if (!that.currentNameSearch()) {
+        return that.allEateries();
+      } else {
+        return ko.utils.arrayFilter(that.allEateries(), function (eatery) {
+          //Matches any word that starts with string
+          var regex = new RegExp("\\b" + that.currentNameSearch(), 'gi');
+          return !(eatery.name().search(regex) == -1);
+        });
+      }
+    });
+
+    this.setCurrentEatery = function (eatery) {
+      //Set eatery as selected
+      that.allEateries().forEach(function (otherEatery) {
+        if (otherEatery.id == eatery.id) {
+          otherEatery.isSelected(true);
+        } else {
+          otherEatery.isSelected(false);
+        }
+      });
+      theMapViewModel.selectMarker(eatery.id);
+    };
+
+    ko.applyBindings(this, document.getElementById('Eateries'));
   }
 };
 
@@ -96,6 +152,7 @@ var MapViewModel = function () {
     _classCallCheck(this, MapViewModel);
 
     this.map;
+    this.infoWindow = {};
     this.markers = [];
     this.bounds = {};
   }
@@ -105,7 +162,12 @@ var MapViewModel = function () {
     value: function initMap() {
       this.map = new google.maps.Map(document.getElementById('map'), {
         center: { lat: 47.606, lng: -122.332 },
-        zoom: 11
+        zoom: 11,
+        mapTypeControl: true,
+        disableDefaultUI: true,
+        zoomControl: true,
+        scrollwheel: false,
+        draggable: true
       });
     }
   }, {
@@ -114,24 +176,119 @@ var MapViewModel = function () {
       var that = this;
       this.bounds = new google.maps.LatLngBounds();
 
-      eateriesObject.data.forEach(function (marker) {
+      eateriesObject.data.forEach(function (marker, index) {
         var position = new google.maps.LatLng(marker.lat(), marker.long());
 
         that.markers.push(new google.maps.Marker({
           position: position,
           map: that.map,
-          animation: google.maps.Animation.DROP
+          animation: google.maps.Animation.DROP,
+          id: index
         }));
 
         that.bounds.extend(position);
       });
 
       this.map.fitBounds(this.bounds);
+      this.map.setZoom(this.map.getZoom() - 1);
+      this.setupInfoWindow();
+    }
+  }, {
+    key: 'resetBounds',
+    value: function resetBounds() {
+      var that = this;
+      this.bounds = new google.maps.LatLngBounds();
+
+      this.markers.forEach(function (marker, index) {
+        that.bounds.extend(marker.position);
+      });
+
+      this.map.fitBounds(this.bounds);
+    }
+  }, {
+    key: 'selectMarker',
+    value: function selectMarker(index) {
+      var that = this;
+      this.infowindow.close();
+      this.markers[index].setAnimation(google.maps.Animation.BOUNCE);
+      setTimeout(function () {
+        that.markers[index].setAnimation(null);
+        that.showInfoWindow(index);
+      }, 700);
+      // this.resetBounds();
+    }
+  }, {
+    key: 'showInfoWindow',
+    value: function showInfoWindow(index) {
+      var content = this.renderInfoWindowContent(index);
+      this.infowindow.setContent(content);
+      this.infowindow.open(this.map, this.markers[index]);
+    }
+  }, {
+    key: 'hideMarkers',
+    value: function hideMarkers() {
+      console.log('hello');
+      console.log(eateriesObject.filterEateries);
+      // marker.setMap(this.map);
+      // marker.setMap(null);
+    }
+  }, {
+    key: 'setupInfoWindow',
+    value: function setupInfoWindow() {
+      var that = this;
+      this.infowindow = new google.maps.InfoWindow({
+        content: '<div>Hello there!</div>'
+      });
+      //Attach event listeners
+      this.markers.forEach(function (marker, index) {
+        marker.addListener('click', function () {
+          var content = that.renderInfoWindowContent(index);
+          that.infowindow.setContent(content);
+          that.infowindow.open(that.map, marker);
+        });
+      });
+    }
+  }, {
+    key: 'renderInfoWindowContent',
+    value: function renderInfoWindowContent(index) {
+      var eateryObjectKO = eateriesObject.data[index];
+      var eatery = {
+        'name': eateryObjectKO.name(),
+        'addressLine1': eateryObjectKO.addressLine1(),
+        'addressLine2': eateryObjectKO.addressLine2(),
+        'website': eateryObjectKO.website(),
+        'logoImage': eateryObjectKO.logoImage(),
+        'rating': eateryObjectKO.rating(),
+        'lat': eateryObjectKO.lat().toFixed(2),
+        'long': eateryObjectKO.long().toFixed(2)
+      };
+      //@see Template Literals: https://goo.gl/oH2hlN
+      return '<section class="EateryInfoWindow">\n              <h4> ' + eatery.name + ' </h4>\n              <address>\n                ' + eatery.addressLine1 + '<br>\n                ' + eatery.addressLine2 + '\n              </address>\n              <a href="' + eatery.website + '" title="Visit ' + eatery.name + ' website">\n                Visit ' + eatery.name + ' website\n              </a>\n              <div>Rating: ' + eatery.rating + '</div>\n              <div>Latitude/Longitude: ' + eatery.lat + '/' + eatery.long + '</div>\n            </section>';
     }
   }]);
 
   return MapViewModel;
 }();
 
+var ErrorMessageHandler = function () {
+  function ErrorMessageHandler() {
+    _classCallCheck(this, ErrorMessageHandler);
+  }
+
+  _createClass(ErrorMessageHandler, [{
+    key: 'displayError',
+    value: function displayError(message) {
+      console.log();
+      // let errorMesssageDiv = document.querySelectorAll('#errorMessage');
+      // errorMesssageDiv.innerHTML(message);
+      $('#errorMessage').html(message);
+      $('#errorModal').foundation('open');
+    }
+  }]);
+
+  return ErrorMessageHandler;
+}();
+
 var eateriesObject = new Eateries();
 var theMapViewModel = new MapViewModel();
+var errorMessageHandler = new ErrorMessageHandler();
--- assets/js/src/app.js
+++ assets/js/src/app.js
@@ -11,10 +11,24 @@ class Eateries {
   }
   getEateriesJSON(){
     let that = this;
+    let defaultMessage = 'Sorry! We could not locate the data. Please try again later.';
+
     jQuery.ajax('assets/js/src/data.json')
+    .fail(function (response) {
+      errorMessageHandler.displayError(defaultMessage);
+    })
     .done(function (data) {
-      that.rawData = JSON.parse(data);
-      that.buildEateriesArray();
+      if (data){
+        try {
+          that.rawData = JSON.parse(data);
+          that.buildEateriesArray();
+        } catch(e) {
+          errorMessageHandler.displayError('Sorry! Parsing Data failed. Please try again later.'); //error in the above string(in this case,yes)!
+          return false;
+        }
+      } else {
+        errorMessageHandler.displayError(defaultMessage);
+      }
     });
   }
   buildEateriesArray() {
@@ -26,14 +40,16 @@ class Eateries {
   buildEatery(business, eateryObject) {
     let that = this;
     that.data.push({
+      'id': that.data.length,
       'name': ko.observable(eateryObject.name),
       'addressLine1': ko.observable(eateryObject.location.address[0]),
-      'addressLine2': ko.observable(eateryObject.location.city),
+      'addressLine2': ko.observable(`${eateryObject.location.city}, ${eateryObject.location.state_code} ${eateryObject.location.postal_code}`),
       'website': ko.observable(business.website),
       'logoImage': ko.observable(business.logo),
       'rating': ko.observable(eateryObject.rating),
       'lat': ko.observable(eateryObject.location.coordinate.latitude),
-      'long': ko.observable(eateryObject.location.coordinate.longitude)
+      'long': ko.observable(eateryObject.location.coordinate.longitude),
+      'isSelected': ko.observable(false)
     });
 
     //All data loaded
@@ -44,11 +60,26 @@ class Eateries {
   }
   getYelpData(business) {
     let that = this;
+    let defaultMessage = 'Sorry! Retrieving Yelp Data failed. Please try again later.';
     jQuery.ajax('api/getYelpBusiness.php', {
       data: { business: business.name }
     })
-    .done(function (eateryObject) {
-      that.buildEatery(business, JSON.parse(eateryObject));
+    .fail(function (response) {
+      errorMessageHandler.displayError(defaultMessage);
+    })
+    .done(function (response) {
+      if (response){
+        let eateryJSON = {};
+        try {
+          eateryJSON = JSON.parse(response);
+        } catch(e) {
+          errorMessageHandler.displayError('Sorry! Parsing Yelp Data failed. Please try again later.'); //error in the above string(in this case,yes)!
+          return false;
+        }
+        that.buildEatery(business, eateryJSON)
+      } else {
+        errorMessageHandler.displayError(defaultMessage);
+      }
     });
   }
 }
@@ -60,8 +91,35 @@ class Eateries {
 var eateriesViewModel = {
   allEateries: ko.observableArray(),
   init: function () {
-    eateriesViewModel.allEateries = ko.observableArray(eateriesObject.data);
-    ko.applyBindings(eateriesViewModel, document.getElementById('Eateries'));
+    let that = this;
+    this.allEateries = ko.observableArray(eateriesObject.data);
+    this.currentNameSearch = ko.observable("");
+
+    this.filterEateries = ko.computed(function() {
+      if(!that.currentNameSearch()) {
+        return that.allEateries();
+      } else {
+        return ko.utils.arrayFilter(that.allEateries(), function(eatery) {
+          //Matches any word that starts with string
+          let regex = new RegExp("\\b" + that.currentNameSearch(), 'gi');
+          return !(eatery.name().search(regex) == -1);
+        });
+      }
+    });
+
+    this.setCurrentEatery = function(eatery) {
+      //Set eatery as selected
+      that.allEateries().forEach(otherEatery => {
+        if(otherEatery.id == eatery.id){
+          otherEatery.isSelected(true);
+        } else {
+          otherEatery.isSelected(false);
+        }
+      });
+      theMapViewModel.selectMarker(eatery.id);
+    };
+
+    ko.applyBindings(this, document.getElementById('Eateries'));
   }
 };
 
@@ -73,27 +131,34 @@ var eateriesViewModel = {
 class MapViewModel {
   constructor() {
     this.map;
+    this.infoWindow = {};
     this.markers = [];
     this.bounds = {};
   }
   initMap(){
     this.map = new google.maps.Map(document.getElementById('map'), {
       center: {lat: 47.606, lng: -122.332},
-      zoom: 11
+      zoom: 11,
+      mapTypeControl: true,
+      disableDefaultUI: true,
+      zoomControl: true,
+      scrollwheel: false,
+      draggable : true
     });
   }
   plotMarkers() {
     let that = this;
     this.bounds = new google.maps.LatLngBounds();
-    
-    eateriesObject.data.forEach(function (marker) {
+
+    eateriesObject.data.forEach(function (marker, index) {
       let position = new google.maps.LatLng(marker.lat(), marker.long());
 
       that.markers.push(
         new google.maps.Marker({
           position: position,
           map: that.map,
-          animation: google.maps.Animation.DROP
+          animation: google.maps.Animation.DROP,
+          id: index
         })
       );
 
@@ -101,9 +166,93 @@ class MapViewModel {
     });
 
     this.map.fitBounds(this.bounds);
+    this.map.setZoom(this.map.getZoom()-1);
+    this.setupInfoWindow();
+  }
+  resetBounds() {
+    let that = this;
+    this.bounds = new google.maps.LatLngBounds();
+
+    this.markers.forEach(function (marker, index) {
+      that.bounds.extend(marker.position);
+    });
+
+    this.map.fitBounds(this.bounds);
+  }
+  selectMarker(index) {
+    let that = this;
+    this.infowindow.close();
+    this.markers[index].setAnimation(google.maps.Animation.BOUNCE);
+    setTimeout(function() {
+      that.markers[index].setAnimation(null);
+      that.showInfoWindow(index);
+    }, 700);
+    // this.resetBounds();
+  }
+  showInfoWindow(index) {
+    let content = this.renderInfoWindowContent(index);
+    this.infowindow.setContent(content)
+    this.infowindow.open(this.map, this.markers[index]);
+  }
+  hideMarkers() {
+    console.log('hello');
+    console.log(eateriesObject.filterEateries);
+    // marker.setMap(this.map);
+    // marker.setMap(null);
+  }
+  setupInfoWindow() {
+    let that = this;
+    this.infowindow = new google.maps.InfoWindow({
+      content: '<div>Hello there!</div>'
+    });
+    //Attach event listeners
+    this.markers.forEach(function (marker, index) {
+      marker.addListener('click', function() {
+        let content = that.renderInfoWindowContent(index);
+        that.infowindow.setContent(content)
+        that.infowindow.open(that.map, marker);
+      });
+    });
+  }
+  renderInfoWindowContent(index) {
+    let eateryObjectKO = eateriesObject.data[index];
+    let eatery = {
+      'name':         eateryObjectKO.name(),
+      'addressLine1': eateryObjectKO.addressLine1(),
+      'addressLine2': eateryObjectKO.addressLine2(),
+      'website':      eateryObjectKO.website(),
+      'logoImage':    eateryObjectKO.logoImage(),
+      'rating':       eateryObjectKO.rating(),
+      'lat':          eateryObjectKO.lat().toFixed(2),
+      'long':         eateryObjectKO.long().toFixed(2)
+    };
+    //@see Template Literals: https://goo.gl/oH2hlN
+    return `<section class="EateryInfoWindow">
+              <h4> ${eatery.name} </h4>
+              <address>
+                ${eatery.addressLine1}<br>
+                ${eatery.addressLine2}
+              </address>
+              <a href="${eatery.website}" title="Visit ${eatery.name} website">
+                Visit ${eatery.name} website
+              </a>
+              <div>Rating: ${eatery.rating}</div>
+              <div>Latitude/Longitude: ${eatery.lat}/${eatery.long}</div>
+            </section>`;
+  }
+}
+
+class ErrorMessageHandler {
+  displayError(message) {
+    console.log();
+    // let errorMesssageDiv = document.querySelectorAll('#errorMessage');
+    // errorMesssageDiv.innerHTML(message);
+    $('#errorMessage').html(message);
+    $('#errorModal').foundation('open');
   }
 }
 
 
 let eateriesObject = new Eateries();
 var theMapViewModel = new MapViewModel();
+let errorMessageHandler = new ErrorMessageHandler();
